a54249b2499908da72a533ff607515fc5c26233e
SirRGB <sirrgb@proton.me>
2026-08-08 16:45:37 +0200

    Revert "aicp: add AppPredictionService to allow list"
    This reverts commit 8ccabbe0422ba9c81142376cfa1d8c4fe2dcbb4a.
    
    Change-Id: I39832e85f0a8d9bed9b309436e36334f1c5fec9f

--- config/frosty_packages.mk
+++ config/frosty_packages.mk
@@ -51,7 +51,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/lib/content-types.properties \
     system/lib/libsepol.so \
     system/lib64/libsepol.so \
-    system/priv-app/AppPredictionService/AppPredictionService.apk \
     system/xbin/wget
 
 # OnDeviceAppPrediction
